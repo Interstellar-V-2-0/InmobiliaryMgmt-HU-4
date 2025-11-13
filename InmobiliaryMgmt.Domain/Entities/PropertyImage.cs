8e1376f2b9f73d98cb847cc973f639ae4f5fdcd1
ZamirE27 <zaesco2001@gmail.com>
2025-11-13 08:58:23 -0500

    We started working in the Domain layer

---
+++ InmobiliaryMgmt.Domain/Entities/PropertyImage.cs
@@ -0,0 +1,10 @@
+namespace InmobiliaryMgmt.Domain.Entities;
+
+public class PropertyImage
+{
+    public int Id { get; set; }
+    public string Url { get; set; }
+    
+    public int PropertyId { get; set; }
+    public Property? Property { get; set; }
+}
